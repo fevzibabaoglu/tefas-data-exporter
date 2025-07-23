5c0db30a41a0bbb9e954cc645457717889c33f11
Fevzi Babaoğlu <fevzibabaoglu03@gmail.com>
2025-07-23 17:12:25 +0300

    chore: update version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tefas-data-exporter",
-    version="0.0.0",
+    version="1.0.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
 )

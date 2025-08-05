27cfd2c57166ef69e2f09580ecbde00d4d986dc4
Fevzi Babaoğlu <fevzibabaoglu03@gmail.com>
2025-08-05 14:40:39 +0300

    chore: bump version to v1.2.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tefas-data-exporter",
-    version="1.2.1",
+    version="1.2.2",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
 )

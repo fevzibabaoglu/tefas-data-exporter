2de08c2dcc8f9ac3aa4337e7d875a436cf617089
Fevzi Babaoğlu <fevzibabaoglu03@gmail.com>
2025-08-05 14:40:04 +0300

    fix: do not fetch any new funds while updating

--- src/data_manager/price_updater.py
+++ src/data_manager/price_updater.py
@@ -21,17 +21,14 @@ import logging
 from dateutil.relativedelta import relativedelta
 from typing import List
 
-from .fund_data_manager import FundDataManager
 from data_struct import Asset, DateRange
-from tefas_requests import FundCodeFetcher, UpdatedPricesFetcher
 from tefas_requests import UpdatedPricesFetcher
 from utils import DateUtils
 
 
 class PriceUpdater:
-    def __init__(self, assets: List[Asset], fund_data_manager: FundDataManager):
+    def __init__(self, assets: List[Asset]):
         self.code_asset_dict = Asset.get_code_asset_dict(assets)
-        self.fund_data_manager = fund_data_manager
 
     def get_last_date(self):
         return max(
@@ -58,24 +55,10 @@ class PriceUpdater:
             f"to {DateUtils.format_date(date_range.get_end_date())}"
         )
 
-        new_asset_codes = []
         new_asset_prices = UpdatedPricesFetcher.fetch_updated_prices(date_range)
         for fund_code, prices in new_asset_prices.items():
             asset = self.code_asset_dict.get(fund_code, None)
             if asset is not None:
                 asset.extend_prices(prices)
-            else:
-                new_asset_codes.append(fund_code)
 
-        assets = list(self.code_asset_dict.values())
-
-        fund_codes_data = {
-            fund_code: founder
-            for fund_code, founder in FundCodeFetcher.fetch_tefas_fund_codes().items()
-            if fund_code in new_asset_codes
-        }
-        if fund_codes_data:
-            new_assets = self.fund_data_manager.fetch_fund_data(fund_codes_data)
-            assets.extend(new_assets)
-
-        return assets
+        return list(self.code_asset_dict.values())
